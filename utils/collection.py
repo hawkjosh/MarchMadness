7b74297e7dd9a6356fd008ad5300f072e1fb0bde
hawkjosh <hawkjosh@icloud.com>
2024-03-21 11:43:47 -0400

    most recent updates...

--- utils/collection.py
+++ utils/collection.py
@@ -9,35 +9,15 @@ def scrapeData(statYear, statDay, statName, statAbbrev):
     soup = BeautifulSoup(response.text, "html.parser")
     table = soup.find("table")
     data = []
-    if statYear == "2024":
-        columns = [
-            "team",
-            "year",
-            statAbbrev,
-            f"{statAbbrev}_last3",
-            f"{statAbbrev}_Home",
-            f"{statAbbrev}_Away",
-        ]
-    else:
-        columns = ["team", "year", statAbbrev]
+    columns = ["team", "year", statAbbrev]
 
     for row in table.find_all("tr")[1:]:
         cells = row.find_all("td")
-        if statYear == "2024":
-            rowData = {
-                "team": cells[1].text.strip(),
-                "year": 2024,
-                statAbbrev: cells[2].text.strip(),
-                f"{statAbbrev}_last3": cells[3].text.strip(),
-                f"{statAbbrev}_Home": cells[5].text.strip(),
-                f"{statAbbrev}_Away": cells[6].text.strip(),
-            }
-        else:
-            rowData = {
-                "team": cells[1].text.strip(),
-                "year": int(statYear),
-                statAbbrev: cells[2].text.strip(),
-            }
+        rowData = {
+            "team": cells[1].text.strip(),
+            "year": int(statYear),
+            statAbbrev: cells[2].text.strip(),
+        }
         data.append(rowData)
 
     return pd.DataFrame(data, columns=columns)
